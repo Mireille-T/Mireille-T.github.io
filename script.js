2ddcfc72cbde953d0daf04add9b150f8232f10d7
Mireille-T <69097387+Mireille-T@users.noreply.github.com>
2022-02-27 13:35:53 +0800

    Include jQuery + bug fixes

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 var toTopButton;
 var upArrow;
 var downArrow;
-var rotatingText;
 var rotatingTextList = ["code!", "tinker!", "3D model!", "solve puzzles!", "learn!"];
 var rotatingImageSrc = ["media/coder_illustration.svg", "media/construction_illustration.svg", "media/3dmodel_illustration.svg", "media/quiz_illustration.svg", "media/learn_illustration.svg"];
 var rotatingImageAlt = ["Coder illustration", "Construction illustration", "3D modelling illustration", "Problem-solving illustration", "Studying illustration"];
 var toggleAnimation;
-var landingImage;
 var counter = 0;
 var animationOn = true;
 
 window.addEventListener('load', (event) => {
     toTopButton = document.getElementById("btn-back-to-top");
-    rotatingText = document.getElementById("rotating");
-    landingImage = document.getElementById("landingIllustration");
     upArrow = document.getElementById("rotating-up");
     downArrow = document.getElementById("rotating-down");
     toggleAnimation = document.getElementById("toggleAnimation");
@@ -31,18 +27,26 @@ function backToTop() {
 
 function scrollNext(){
     counter++;
-    if (counter == 5) counter = 0;
-    rotatingText.textContent = rotatingTextList[counter];
-    landingImage.src = rotatingImageSrc[counter];
-    landingImage.alt = rotatingImageAlt[counter];
+    if (counter >= 5) counter = 0;
+    $("#rotating").fadeTo(200,0.30, function() {
+        $("#rotating").text(rotatingTextList[counter]);
+    }).fadeTo(500,1);
+    $("#landingIllustration").fadeTo(200,0.30, function() {
+        $("#landingIllustration").attr("src", rotatingImageSrc[counter]);
+        $("#landingIllustration").attr("alt", rotatingImageAlt[counter]);
+    }).fadeTo(200,1);
 }
 
 function scrollPrevious(){
     counter--;
-    if (counter == -1) counter = 4;
-    rotatingText.textContent = rotatingTextList[counter];
-    landingImage.src = rotatingImageSrc[counter];
-    landingImage.alt = rotatingImageAlt[counter];
+    if (counter <= -1) counter = 4;
+    $("#rotating").fadeTo(200,0.30, function() {
+        $("#rotating").text(rotatingTextList[counter]);
+    }).fadeTo(500,1);
+    $("#landingIllustration").fadeTo(200,0.30, function() {
+        $("#landingIllustration").attr("src", rotatingImageSrc[counter]);
+        $("#landingIllustration").attr("alt", rotatingImageAlt[counter]);
+    }).fadeTo(200,1);
 }
 
 function toggleAnimate(){
