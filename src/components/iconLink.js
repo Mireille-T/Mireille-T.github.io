258e57be64fc5dafb99686a2db11e628c79bcd3f
Mireille-T <mireille.tan@gmail.com>
2024-08-14 22:54:52 +0800

    Deploy React build

--- src/components/iconLink.js
+++
@@ -1,7 +0,0 @@
-export default function IconLink({ link, ariaLabel, iconClass }) {
-    return (
-        <a href={link} target="_blank" rel="noreferrer" aria-label={ariaLabel}><i
-            className={iconClass}></i></a>
-    );
-
-}
